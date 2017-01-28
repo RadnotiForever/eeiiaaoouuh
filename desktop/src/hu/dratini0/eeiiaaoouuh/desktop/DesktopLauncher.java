3c7945c9ffd036bc0ca78f4ecd14991932facc4f
Balint Kovacs <dratini0@gmail.com>
2017-01-28 11:14:37 +0000

    Set up project

---
+++ desktop/src/hu/dratini0/eeiiaaoouuh/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.dratini0.eeiiaaoouuh.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.dratini0.eeiiaaoouuh.VoiceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VoiceGame(), config);
+	}
+}
